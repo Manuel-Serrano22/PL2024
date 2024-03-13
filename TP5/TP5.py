3b0044da04b5d5a921473fc9729100f14a562426
Manuel-Serrano22 <manser03@gmail.com>
2024-03-13 22:40:06 +0000

    feito

--- TP5/TP5.py
+++ TP5/TP5.py
@@ -5,72 +5,18 @@ from decimal import Decimal
 import ply.lex as lex
 
 
-produtos = {1:("Coca-Cola", 2.50), 2:("Pepsi", 1.20), 3:("Sprite", 1.40),
-            4:("Batatas Fritas", 1.00), 5:("Bolachas", 0.80), 6:("Cafe", 0.70),
-            7:("Chocolate", 2.10)}
+produtos = {1:("Coca-Cola", Decimal("2.50")), 2:("Pepsi", Decimal("1.20")), 3:("Sprite", Decimal("1.40")),
+            4:("Batatas Fritas", Decimal("1.00")), 5:("Bolachas", Decimal("0.80")), 6:("Cafe", Decimal("0.70")),
+            7:("Chocolate", Decimal("2.10"))}
 
 tokens = (
     "Listar",
-    "PalavraMoeda",
     "Moeda",
     "Selecionar",
-    "Sair"
-    "Delimitador",
+    "Sair",
 )
 
 somadorMoedas = 0
-
-def t_Listar(t):
-    r"(?i)\bListar\b"
-    for i in range(1, len(produtos)+1):
-        print("Produto(id = " + str(i) + ") " + produtos[i][0] + " - " + str(produtos[i][1]) + "€")
-
-def t_PalavraMoeda(t):
-   r"(?i)\bMoeda\b"
-
-def t_Moeda(t):
-    r"1c|2c|5c|10c|20c|50c|1e|2e"
-    global somadorMoedas
-    if re.match("1c", t.value):
-        somadorMoedas += Decimal('0.01')
-    elif re.match("2c", t.value):
-        somadorMoedas += Decimal('0.02')
-    elif re.match("5c", t.value):
-        somadorMoedas += Decimal('0.05')
-    elif re.match("10c", t.value):
-        somadorMoedas += Decimal('0.10')
-    elif re.match("20c", t.value):
-        somadorMoedas += Decimal('0.20')
-    elif re.match("50c", t.value):
-        somadorMoedas += Decimal('0.50')
-    elif re.match("1e", t.value):
-        somadorMoedas += Decimal('1.00')
-    elif re.match("2e", t.value):
-        somadorMoedas += Decimal('2.00')
-
-def t_Selecionar(t):
-    r"(?i)\bSelecionar\b\s+\d+"
-    global somadorMoedas
-    identificador = re.search(r"(\d+)", t.value).group(1)
-    if(int(identificador) > len(produtos)):
-        print("Não existe esse produto")
-    else:
-        produto = produtos[int(identificador)][0]
-        valor = produtos[int(identificador)][1]
-
-        if(valor > somadorMoedas):
-            print("O valor do " + produto + " é: " + str(valor) + "€")
-        else:
-            print("O produto " + produto + " foi selecionado")
-            somadorMoedas -= Decimal(str(valor))
-
-
-def t_Sair(t):
-    r"(?i)\bSair\b"
-    print(t.value)
-
-def t_Delimitador(t):
-    r","
 
 def substituir(match):
     if(re.match(r"^0", match.group(1))):
@@ -90,12 +36,97 @@ def substituir(match):
             novoSaldo = euros
     return novoSaldo
 
+def saldoUtilizador(valor):
+    novoSaldo = re.sub(r"(\d+)\.(\d+)", substituir, str(valor))
+    print("Saldo: ", novoSaldo)
+    return novoSaldo
+
+def t_Listar(t):
+    r"(?i)\bListar\b"
+    for i in range(1, len(produtos)+1):
+        print("Produto(id = " + str(i) + ") " + produtos[i][0] + " - " + str(produtos[i][1]) + "€")
+
+def t_Moeda(t):
+    r"(?i)moeda\s+(?:(1c|2c|5c|10c|20c|50c|1e|2e),\s+)*(1c|2c|5c|10c|20c|50c|1e|2e)\s+\.$"
+    listaMoedas = re.findall(r"(1c)|(2c)|(5c)|(10c)|(20c)|(50c)|(1e)|(2e)", t.value)
+    global somadorMoedas
+    for _1c, _2c, _5c, _10c, _20c, _50c, _1e, _2e in listaMoedas:
+        if _1c:
+            somadorMoedas += Decimal('0.01')
+        elif _2c:
+            somadorMoedas += Decimal('0.02')
+        elif _5c:
+            somadorMoedas += Decimal('0.05')
+        elif _10c:
+            somadorMoedas += Decimal('0.10')
+        elif _20c:
+            somadorMoedas += Decimal('0.20')
+        elif _50c:
+            somadorMoedas += Decimal('0.50')
+        elif _1e:
+            somadorMoedas += Decimal('1.00')
+        elif _2e:
+            somadorMoedas += Decimal('2.00')
+    saldoUtilizador(somadorMoedas)
+
+def t_Selecionar(t):
+    r"(?i)\bSelecionar\b\s+\d+"
+    global somadorMoedas
+    identificador = re.search(r"(\d+)", t.value).group(1)
+    if(int(identificador) > len(produtos)):
+        print("Não existe esse produto")
+    else:
+        produto = produtos[int(identificador)][0]
+        valor = produtos[int(identificador)][1]
+
+        if(valor > somadorMoedas):
+            print("O valor do " + produto + " é: " + saldoUtilizador(valor))
+            saldoUtilizador(somadorMoedas)
+        else:
+            print("O produto " + produto + " foi selecionado")
+            somadorMoedas -= valor
+            saldoUtilizador(somadorMoedas)
+
+def calcularTroco():
+    moedas = [1, 2, 5, 10, 20, 50, 100, 200]  # em centimos
+    global somadorMoedas
+    saldoUtilizador = somadorMoedas
+    saldoUtilizadorCentimos = int(saldoUtilizador * 100)
+    troco = {}
+
+    for moeda in reversed(moedas):
+        while saldoUtilizadorCentimos >= moeda:
+            quantidade_moedas = saldoUtilizadorCentimos // moeda
+            if(moeda not in troco):
+                troco[moeda] = quantidade_moedas
+            else:
+                troco[moeda] += quantidade_moedas
+            saldoUtilizadorCentimos -= quantidade_moedas * moeda
+
+    print(troco)
+    return troco
+
+def formatarTroco(troco):
+    troco_formatado = ""
+    for moeda in troco.keys():
+        quantidade = troco[moeda]
+        if quantidade > 0:
+            if moeda >= 100:
+                troco_formatado += f"{quantidade}x {moeda // 100}e "
+            else:
+                troco_formatado += f"{quantidade}x {moeda}c "
+    return troco_formatado.strip()
+
+def t_Sair(t):
+    r"(?i)\bSair\b"
+    troco = calcularTroco()
+    trocoNovo = formatarTroco(troco)
+    print(f"Pode retirar o troco: {trocoNovo}")
+    print("Até à próxima.")
+
+
 def t_newline(t):
     r"\n+"
-    global somadorMoedas
-    if(somadorMoedas != 0):
-        novoSaldo = re.sub(r"(\d+)\.(\d+)", substituir, str(somadorMoedas))
-        print("Saldo: ", novoSaldo)
     t.lexer.lineno += len(t.value)
 
 
@@ -106,6 +137,8 @@ def t_error(t):
     t.lexer.skip(1)
 
 def main():
+    print("2024-03-08, Stock carregado, Estado atualizado.")
+    print("Bom dia. Estou disponível para atender o seu pedido.")
     lexer = lex.lex()
     for linha in sys.stdin:
         lexer.input(linha)
